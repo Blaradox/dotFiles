493a80ec509ae20f84a171f0e9922e935eb8c792
Austin Sloane <sloane.ats@gmail.com>
2018-04-21 19:38:45 -0700

    add function to fuzzy find music in mpd

--- zsh/.funcs.sh
+++ zsh/.funcs.sh
@@ -74,3 +74,12 @@ bro() {
     open "$url"
   fi
 }
+
+fmpc() {
+  local song_position
+  song_position=$(mpc -f "%position%) %artist% - %title%" playlist | \
+    fzf-tmux --query="$1" --reverse --select-1 --exit-0 | \
+    sed -n 's/^\([0-9]*\)).*/\1/p') || return 1
+  [ -n "$song_position" ] && mpc -q play $song_position
+}
+
