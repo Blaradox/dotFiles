4b15ae1923439398cf13035cdb4a2211af5a75c2
Austin Sloane <sloane.ats@gmail.com>
2019-02-21 20:21:48 -0800

    add aliases and functions for 'cheat.sh','wttr.in'

--- shell/.funcs.sh
+++ shell/.funcs.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
-# PATH
+## Aliases
+alias g="git"
+alias ls="ls -GFh"
+alias egrep="egrep --color=auto"
+alias ping="prettyping --nolegend"
+alias preview="fzf --preview 'bat --line-range :100 --color always {}'"
+alias du="ncdu --color 'dark' -rr -x --exclude '.git' --exclude 'node_modules'"
+alias ncdu="ncdu --color 'dark' -x"
+
+## PATH
 export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin
 export PATH=$PATH:/usr/local/opt/fzf/bin
 export PATH=$PATH:~/.local/bin
 export PATH=$PATH:/usr/sbin:/sbin
 
-# Aliases
-alias ls="ls -GFh"
-alias egrep="egrep --color=auto"
-alias weather="curl wttr.in"
-alias cheat="curl cheat.sh"
-
-# Variables
+## Variables
 export VISUAL=/usr/local/bin/vim
 export EDITOR=$VISUAL
 export DEFAULT_USER=sloaneat
 
-# FZF C-r and C-t
+## FZF C-r and C-t
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow \
   --glob "!{.git,node_modules}/*" \
@@ -25,14 +28,11 @@ export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow \
 export FZF_DEFAULT_OPTS='--color=16'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-# Shortcut Sync
-source ~/.bash_shortcuts
-
 # tmuxp Completions not working yet in zsh...
 # https://github.com/tmux-python/tmuxp/issues/190
 # eval "$(_TMUXP_COMPLETE=source tmuxp)"
 
-# Functions
+## Functions
 # https://github.com/junegunn/fzf/wiki/Examples
 fd() {
   local dir
@@ -40,10 +40,10 @@ fd() {
 }
 
 # CTRL-O to open with `open` command,
-# CTRL-E or Enter key to open with the $EDITOR
+# Enter key to open with the $EDITOR
 fe() {
   local out file key
-  IFS=$'\n' out=($(fzf --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+  IFS=$'\n' out=($(fzf --preview="bat --color 'always' {}" --query="$1" --exit-0 --expect=ctrl-o))
   key=$(head -1 <<< "$out")
   file=$(head -2 <<< "$out" | tail -1)
   if [ -n "$file" ]; then
@@ -92,3 +92,23 @@ fmpc() {
   [ -n "$song_position" ] && mpc -q play $song_position
 }
 
+weather() {
+  # Require `curl` for this
+  if ! command -v curl &>/dev/null; then
+    printf "%s\n" "[ERROR] weather: This command requires 'curl', please install it."
+    return 1
+  fi
+  if [[ $1 -gt 0 ]]; then format="?format=$1"; fi
+  curl -m 10 "http://wttr.in/${format:-San+Francisco}" 2>/dev/null || \
+    printf "%s\n" "[ERROR] weather: Could not connect to weather service."
+}
+
+cheat() {
+  if ! command -v curl &>/dev/null; then
+    printf "%s\n" "[ERROR] cheat: This command requires 'curl', please install it."
+    return 1
+  fi
+  topic=$1
+  shift
+  curl -m 10 "http://cheat.sh/${topic}/$*"
+}
