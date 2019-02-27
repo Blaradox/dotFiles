8ac79a2acc2c3e010646cd78b5a14d74a37a6b45
Austin Sloane <sloane.ats@gmail.com>
2019-02-27 15:35:54 -0800

    change aliases for git status and stash

--- shell/.alias.bash
+++ shell/.alias.bash
@@ -13,9 +13,6 @@ alias ncdu="ncdu --color 'dark' -x"
 # Version control (taken from Prezto git module)
 # https://github.com/sorin-ionescu/prezto/blob/master/modules/git/alias.zsh
 alias g="git"
-# Status (s)
-alias gs="git status --short --branch --ignored"
-alias gsl="git status --long --verbose --ignored"
 # Branch (b)
 alias gb="git branch"
 alias gba="git branch --all --verbose"
@@ -34,8 +31,10 @@ alias gfr="git pull --rebase"
 # Index (i)
 alias gia="git add"
 alias gid="git diff --no-ext-diff --cached"
-alias gix='git rm -r --cached'
-alias giX='git rm -rf --cached'
+alias gis="git status --short --branch --ignored"
+alias gisl="git status --long --verbose --ignored"
+alias gix="git rm -r --cached"
+alias giX="git rm -rf --cached"
 # Log (l)
 alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
 alias gls='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
@@ -46,24 +45,24 @@ alias gma="git merge --abort"
 # Push (p)
 alias gp="git push"
 # Rebase (r)
-alias gr='git rebase'
-alias gra='git rebase --abort'
-alias grc='git rebase --continue'
-alias gri='git rebase --interactive'
-alias grs='git rebase --skip'
+alias gr="git rebase"
+alias gra="git rebase --abort"
+alias grc="git rebase --continue"
+alias gri="git rebase --interactive"
+alias grs="git rebase --skip"
 # Remote (R)
-alias gR='git remote'
-alias gRl='git remote --verbose'
-alias gRa='git remote add'
-alias gRx='git remote rm'
-alias gRm='git remote rename'
-alias gRu='git remote update'
-alias gRp='git remote prune'
-alias gRs='git remote show'
+alias gR="git remote"
+alias gRl="git remote --verbose"
+alias gRa="git remote add"
+alias gRx="git remote rm"
+alias gRm="git remote rename"
+alias gRu="git remote update"
+alias gRp="git remote prune"
+alias gRs="git remote show"
 # Stash (s)
-alias gS='git stash'
-alias gSa='git stash apply'
-alias gSx='git stash drop'
-alias gSX='git-stash-clear-interactive'
-alias gSl='git stash list'
-alias gSL='git-stash-dropped'
+alias gs="git stash"
+alias gsa="git stash apply"
+alias gsx="git stash drop"
+alias gsX="git-stash-clear-interactive"
+alias gsl="git stash list"
+alias gsL="git-stash-dropped"
