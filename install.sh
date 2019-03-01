6bf00503ed361f96dbe67ea1e90fb8b0b463fbee
Austin Sloane <sloane.ats@gmail.com>
2019-02-28 19:14:17 -0800

    add Hack font w/ slashed zero and fix install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
-echo "Stowing Dotfiles..."
+if [[ -s $HOME/dotfiles ]]; then
 
-cd ~/dotFiles
-for file in ~/dotFiles/*; do
-  # Only run stow on directories
-  if [ -d ${file} ]; then
-    stow -R $(basename $file)
-    echo "$(basename $file) stowed."
-  fi
-done
+  echo "Stowing Dotfiles..."
 
-# stow by default ignores .gitignore files
-ln -sf ~/dotfiles/git/.gitignore ~/.gitignore
+  cd "$HOME/dotFiles"
+  # Only choose desired configurations
+  configs=(fonts git mpd ncmpcpp ranger scripts shell tmux vim)
+  for file in ${configs[@]}; do
+    # Only run stow on directories
+    if [[ -d $file ]]; then
+      stow -R "$(basename $file)"
+      echo "$(basename $file) stowed."
+    fi
+  done
 
-echo "Done Stowing!"
+  # stow by default ignores .gitignore files
+  ln -sf "$HOME/dotfiles/git/.gitignore" "$HOME/.gitignore"
 
+  echo "Done Stowing!"
+else
+  echo "Check to make sure that you cloned this repository in your home folder"
+fi
