bd09fe2d8e247e73507a17a50d2ebe5b0191aada
Austin Sloane <sloane.ats@gmail.com>
2019-03-13 14:10:50 -0700

    use brew version of ruby and fzf follows symlinks

--- shell/.vars.bash
+++ shell/.vars.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # PATH
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/bin
+export PATH=/usr/local/bin:/usr/local/sbin
+export PATH=$PATH:/usr/local/opt/ruby/bin
+export PATH=$PATH:/usr/bin:/bin
 export PATH=$PATH:/usr/local/opt/fzf/bin
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/usr/sbin:/sbin
@@ -12,6 +14,6 @@ export VISUAL=/usr/local/bin/vim
 export EDITOR=$VISUAL
 
 # FZF
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden --follow'
 export FZF_DEFAULT_OPTS='--color=16'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
