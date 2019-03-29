85954fb37873e09db40dc2e83318c9499d768e09
Austin Sloane <sloane.ats@gmail.com>
2019-03-29 15:05:33 -0700

    install script will change based on $OSTYE

--- install.sh
+++ install.sh
@@ -1,24 +1,35 @@
 #!/usr/bin/env bash
 
-if [[ -s "$HOME/dotFiles" ]]; then
+function stow-dots {
+  local configs=()
+  if [[ $OSTYPE == darwin* ]]; then
+    configs=(fonts git karabiner kitty mpd ncmpcpp scripts shell tmux vim)
+  elif [[ $OSTYPE == linux* ]]; then
+    configs=(fonts git kitty mpd ncmpcpp scripts shell tmux vim)
+  fi
 
-  echo "Stowing Dotfiles..."
+  mkdir -p "$HOME/.local/share/fonts"
+  mkdir -p "$HOME/.local/bin"
+  mkdir -p "$HOME/.config"
 
+  printf "Stowing Dotfiles...\n"
   cd "$HOME/dotFiles"
-  # Only choose desired configurations
-  configs=(fonts git karabiner kitty mpd ncmpcpp scripts shell tmux vim)
   for file in ${configs[@]}; do
     # Only run stow on directories
     if [[ -d "$file" ]]; then
       stow -R "$(basename $file)"
-      echo "$(basename $file) stowed."
+      printf "$(basename $file) stowed.\n"
     fi
   done
 
   # stow by default ignores .gitignore files
   ln -sf "$HOME/dotFiles/.ignore" "$HOME/.ignore"
 
-  echo "Done Stowing!"
+  printf "Done Stowing!\n"
+}
+
+if [[ -s "$HOME/dotFiles" ]]; then
+  stow-dots "$@"
 else
-  echo "Check to make sure that you cloned this repository in your home folder"
+  printf "Check to make sure that you cloned this repository in your home folder\n"
 fi
