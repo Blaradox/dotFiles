82aacf5f09a8fd8215163233579f065e406ed612
Austin Sloane <sloane.ats@gmail.com>
2021-03-26 15:51:43 -0700

    fix misspelling in install script

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ function install_yay() {
 function install_vim_plug() {
   echo "Installing vim_plug..."
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim_plug/master/plug.vim'
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 }
 
 function stow_dots() {
